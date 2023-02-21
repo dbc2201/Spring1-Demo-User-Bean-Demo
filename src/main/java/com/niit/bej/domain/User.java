6c39dea054a85d0c7fd4b7c7b06941718ed6dd30
Divyansh Bhardwaj <divyansh.bhardwaj@niit.com>
2023-02-21 12:28:19 +0530

    added toString() for User

--- src/main/java/com/niit/bej/domain/User.java
+++ src/main/java/com/niit/bej/domain/User.java
@@ -57,4 +57,13 @@ public class User {
     public int hashCode() {
         return Objects.hash(username, password, order);
     }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", order=" + order +
+                '}';
+    }
 }
