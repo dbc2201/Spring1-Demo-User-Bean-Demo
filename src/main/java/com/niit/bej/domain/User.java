28f2b24fdbf5baaa770fb3d3f4a96000f0253137
Divyansh Bhardwaj <divyansh.bhardwaj@niit.com>
2023-02-21 12:26:23 +0530

    added constructors for User

--- src/main/java/com/niit/bej/domain/User.java
+++ src/main/java/com/niit/bej/domain/User.java
@@ -9,4 +9,13 @@ public class User {
     private String username;
     private String password;
     private ProductOrder order;
+
+    public User(String username, String password, ProductOrder order) {
+        this.username = username;
+        this.password = password;
+        this.order = order;
+    }
+
+    public User() {
+    }
 }
