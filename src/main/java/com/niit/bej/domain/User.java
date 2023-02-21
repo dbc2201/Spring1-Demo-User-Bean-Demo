451288b0923eeba704e0f2a3ed576b96dff17386
Divyansh Bhardwaj <divyansh.bhardwaj@niit.com>
2023-02-21 12:26:48 +0530

    added getters and setters for User

--- src/main/java/com/niit/bej/domain/User.java
+++ src/main/java/com/niit/bej/domain/User.java
@@ -18,4 +18,28 @@ public class User {
 
     public User() {
     }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public ProductOrder getOrder() {
+        return order;
+    }
+
+    public void setOrder(ProductOrder order) {
+        this.order = order;
+    }
 }
