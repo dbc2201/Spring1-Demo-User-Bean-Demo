13b3a781c5765d983102cf87fe1ee1b91e40e256
Divyansh Bhardwaj <divyansh.bhardwaj@niit.com>
2023-02-21 12:24:22 +0530

    created ProductOrder.java

---
+++ src/main/java/com/niit/bej/domain/ProductOrder.java
@@ -0,0 +1,9 @@
+/*
+ * Author Name: Divyansh Bhardwaj
+ * Date: 21-02-2023
+ * Created With: IntelliJ IDEA Community Edition
+ */
+package com.niit.bej.domain;
+
+public class ProductOrder implements Order {
+}
