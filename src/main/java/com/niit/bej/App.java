8d2e139e6359a98723a0672bb67a9c3980a629e1
Divyansh Bhardwaj <divyansh.bhardwaj@niit.com>
2023-02-21 12:40:40 +0530

    created a User bean

--- src/main/java/com/niit/bej/App.java
+++ src/main/java/com/niit/bej/App.java
@@ -1,10 +1,17 @@
 package com.niit.bej;
 
+import com.niit.bej.bean.BeanConfig;
+import com.niit.bej.domain.User;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(BeanConfig.class);
+        User user = applicationContext.getBean(User.class);
+        System.out.println(user.getUsername());
     }
 }
