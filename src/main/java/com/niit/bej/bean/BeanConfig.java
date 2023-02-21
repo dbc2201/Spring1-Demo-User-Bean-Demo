75da442eadd3b359c664e5da77251b1410c1eca0
Divyansh Bhardwaj <divyansh.bhardwaj@niit.com>
2023-02-21 12:37:00 +0530

    added methods for creating beans of User and ProductOrder

--- src/main/java/com/niit/bej/bean/BeanConfig.java
+++ src/main/java/com/niit/bej/bean/BeanConfig.java
@@ -5,5 +5,19 @@
  */
 package com.niit.bej.bean;
 
+import com.niit.bej.domain.ProductOrder;
+import com.niit.bej.domain.User;
+import org.springframework.context.annotation.Bean;
+
 public class BeanConfig {
+
+    @Bean
+    public ProductOrder createProductOrder() {
+        return new ProductOrder(1, 1000.0);
+    }
+
+    @Bean
+    public User createUser() {
+        return new User("dbc2201", "123ghj79", createProductOrder());
+    }
 }
